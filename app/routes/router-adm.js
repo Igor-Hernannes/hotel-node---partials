4fe02ef8a701bc335a9ba661986b70421ff31b7e
Igor Hernannes <rm98782@estudante.fieb.edu.br>
2026-08-11 12:30:00 -0300

    aboba v2

--- app/routes/router-adm.js
+++ app/routes/router-adm.js
@@ -10,9 +10,18 @@ router.get("/adm-cliente", (req, res)=>{
     res.render("pages/adm-cliente");
 })
 
+//alterado o render
 router.get("/adm-cliente-novo", (req, res)=>{
-    res.render("pages/adm-cliente-novo", {"erros": null, "valores": {"nome":"", "email":"", }});
+    res.render("pages/adm-cliente-novo", {"erros": null, "valores": {"nome":"", "cep":"" ,"email":"", "senha":""} ,"retorno":null });
 })
+
+//post do router adm novo
+router.post(
+    "/adm-cliente-novo",
+
+    body("nome").isLength({ min : 3, max : 30})
+    .withMessage
+)
 
 router.get("/adm-cliente-edit", (req, res)=>{
     res.render("pages/adm-cliente-edit");
